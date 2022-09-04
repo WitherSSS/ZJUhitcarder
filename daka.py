d99a60d4726811f7ec73a42c7374bb2a252cf8a3
WitherSSS <68819438+WitherSSS@users.noreply.github.com>
2022-09-04 20:15:07 +0800

    Update daka.py

--- daka.py
+++ daka.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
-import requests, json, re
-import time, datetime, os, sys
+import requests
+import json
+import re
+import datetime
+import time
 import ddddocr
+
 
 class DaKa(object):
     """Hit card class
@@ -11,21 +15,29 @@ class DaKa(object):
         login_url: (str) 登录url
         base_url: (str) 打卡首页url
         save_url: (str) 提交打卡url
+        self.headers: (dir) 请求头
         sess: (requests.Session) 统一的session
     """
+
     def __init__(self, username, password):
         self.username = username
         self.password = password
         self.login_url = "https://zjuam.zju.edu.cn/cas/login?service=https%3A%2F%2Fhealthreport.zju.edu.cn%2Fa_zju%2Fapi%2Fsso%2Findex%3Fredirect%3Dhttps%253A%252F%252Fhealthreport.zju.edu.cn%252Fncov%252Fwap%252Fdefault%252Findex"
         self.base_url = "https://healthreport.zju.edu.cn/ncov/wap/default/index"
         self.save_url = "https://healthreport.zju.edu.cn/ncov/wap/default/save"
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
+        }
         self.sess = requests.Session()
+        self.captcha_url = 'https://healthreport.zju.edu.cn/ncov/wap/default/code'
 
     def login(self):
         """Login to ZJU platform"""
-        res = self.sess.get(self.login_url)
-        execution = re.search('name="execution" value="(.*?)"', res.text).group(1)
-        res = self.sess.get(url='https://zjuam.zju.edu.cn/cas/v2/getPubKey').json()
+        res = self.sess.get(self.login_url, headers=self.headers)
+        execution = re.search(
+            'name="execution" value="(.*?)"', res.text).group(1)
+        res = self.sess.get(
+            url='https://zjuam.zju.edu.cn/cas/v2/getPubKey', headers=self.headers).json()
         n, e = res['modulus'], res['exponent']
         encrypt_password = self._rsa_encrypt(self.password, e, n)
 
@@ -35,38 +47,45 @@ class DaKa(object):
             'execution': execution,
             '_eventId': 'submit'
         }
-        res = self.sess.post(url=self.login_url, data=data)
+        res = self.sess.post(url=self.login_url,
+                             data=data, headers=self.headers)
 
         # check if login successfully
         if '统一身份认证' in res.content.decode():
             raise LoginError('登录失败，请核实账号密码重新登录')
         return self.sess
-    
+
     def post(self):
         """Post the hitcard info"""
-        res = self.sess.post(self.save_url, data=self.info)
+        res = self.sess.post(self.save_url, data=self.info,
+                             headers=self.headers)
         return json.loads(res.text)
-    
+
     def get_date(self):
         """Get current date"""
         today = datetime.date.today()
-        return "%4d%02d%02d" %(today.year, today.month, today.day)
-        
+        return "%4d%02d%02d" % (today.year, today.month, today.day)
+
     def get_info(self, html=None):
         """Get hitcard info, which is the old info with updated new time."""
         if not html:
-            res = self.sess.get(self.base_url)
+            res = self.sess.get(self.base_url, headers=self.headers)
             html = res.content.decode()
-        
+
         try:
-            old_info = json.loads(re.findall(r'oldInfo: ({[^\n]+})', html)[0])
+            old_infos = re.findall(r'oldInfo: ({[^\n]+})', html)
+            if len(old_infos) != 0:
+                old_info = json.loads(old_infos[0])
+            else:
+                raise RegexMatchError("未发现缓存信息，请先至少手动成功打卡一次再运行脚本")
+
             new_info_tmp = json.loads(re.findall(r'def = ({[^\n]+})', html)[0])
             new_id = new_info_tmp['id']
             name = re.findall(r'realname: "([^\"]+)",', html)[0]
             number = re.findall(r"number: '([^\']+)',", html)[0]
-        except IndexError as err:
+        except IndexError:
             raise RegexMatchError('Relative info not found in html with regex')
-        except json.decoder.JSONDecodeError as err:
+        except json.decoder.JSONDecodeError:
             raise DecodeError('JSON decode error')
 
         new_info = old_info.copy()
@@ -75,6 +94,10 @@ class DaKa(object):
         new_info['number'] = number
         new_info["date"] = self.get_date()
         new_info["created"] = round(time.time())
+        new_info["address"] = "浙江省杭州市西湖区"
+        new_info["area"] = "浙江省 杭州市 西湖区"
+        new_info["province"] = new_info["area"].split(' ')[0]
+        new_info["city"] = new_info["area"].split(' ')[1]
         # form change
         new_info['jrdqtlqk[]'] = 0
         new_info['jrdqjcqk[]'] = 0
@@ -84,35 +107,39 @@ class DaKa(object):
         new_info['jcqzrq'] = ""
         new_info['gwszdd'] = ""
         new_info['szgjcs'] = ""
-        new_info["verifyCode"] = self.get_captcha()
+        new_info["internship"] = 1
+
+        ocr = ddddocr.DdddOcr(show_ad=False)
+        resp = self.sess.get(self.captcha_url, headers=self.headers)
+        captcha = ocr.classification(resp.content)
+        new_info['verifyCode'] = captcha
+        # 2021.08.05 Fix 2
+        magics = re.findall(r'"([0-9a-f]{32})":\s*"([^\"]+)"', html)
+        for item in magics:
+            new_info[item[0]] = item[1]
+
         self.info = new_info
         return new_info
 
     def _rsa_encrypt(self, password_str, e_str, M_str):
-        password_bytes = bytes(password_str, 'ascii') 
+        password_bytes = bytes(password_str, 'ascii')
         password_int = int.from_bytes(password_bytes, 'big')
-        e_int = int(e_str, 16) 
-        M_int = int(M_str, 16) 
-        result_int = pow(password_int, e_int, M_int) 
+        e_int = int(e_str, 16)
+        M_int = int(M_str, 16)
+        result_int = pow(password_int, e_int, M_int)
         return hex(result_int)[2:].rjust(128, '0')
 
-    def get_captcha(self):
-      """Get captcha using ddddocr"""
-      captcha_url = 'https://healthreport.zju.edu.cn/ncov/wap/default/code'
-      ocr = ddddocr.DdddOcr()
-      resp = self.sess.get(captcha_url)
-      captcha = ocr.classification(resp.content)
-      return captcha
-    
 
-# Exceptions 
+# Exceptions
 class LoginError(Exception):
     """Login Exception"""
     pass
 
+
 class RegexMatchError(Exception):
     """Regex Matching Exception"""
     pass
+
 
 class DecodeError(Exception):
     """JSON Decode Exception"""
